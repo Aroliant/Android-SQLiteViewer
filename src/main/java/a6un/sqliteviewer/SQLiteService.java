c36adcf04cca1e05cf2cf5f3e454bb234b7acda8
Jacob Samro <admin@jsam.in>
2017-03-16 00:02:50 +0530

    Simple Service Initialized

--- src/main/java/a6un/sqliteviewer/SQLiteService.java
+++ src/main/java/a6un/sqliteviewer/SQLiteService.java
@@ -1,21 +1,29 @@
 package a6un.sqliteviewer;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 /**
  * Created by Jacob Samro on 03-Mar-17.
  */
 public class SQLiteService extends Service {
 
-    static String databases[];
+    static ArrayList<String> databases = new ArrayList<String>();
+    static Context context;
 
 
-    private SQLiteService(String databases[]) {
+    public SQLiteService(Context context,ArrayList<String> databases) {
 
+        this.context = context;
         this.databases = databases;
+
+        Log.d("DATABASES : " , databases.toString());
 
     }
 
